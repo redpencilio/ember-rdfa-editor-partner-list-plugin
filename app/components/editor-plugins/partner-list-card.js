6f09a601d7a859214822f29130d649b47a272438
Mehran Naghizadeh <tarhineh@gmail.com>
2019-09-05 11:56:50 +0200

    Generate the plugin scaffold

---
+++ app/components/editor-plugins/partner-list-card.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-partner-list-plugin/components/editor-plugins/partner-list-card';
---
+++ app/services/rdfa-editor-partner-list-plugin.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-partner-list-plugin/services/rdfa-editor-partner-list-plugin';
